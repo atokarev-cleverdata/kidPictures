4f6002c5f330915b9487d93f31a384bf644f9efe
Alexander Tokarev <alex.tokarew@gmail.com>
2019-02-23 10:42:02 +0300

    Mapping latin letters to russian

--- js/main.js
+++ js/main.js
@@ -1,4 +1,35 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-	console.log("Loading ducument")
-  //do work
+document.addEventListener("DOMContentLoaded", function(event) {
+	var pictures = [
+		'ПЯТАЧОК.jpg',
+		 'СОВА.png'
+	];
+
+	var engKeys = "QWERTYUIOP[]ASDFGHJKL;'ZXCVBNM,."
+	var rusKeys = "ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ"
+
+	function keyToLetter(key) {
+		var upcaseKey = key.toUpperCase();
+		var keyIndex = engKeys.indexOf(upcaseKey);
+		return (keyIndex == -1) ? upcaseKey : rusKeys[keyIndex];
+	};
+
+	function loadPictures() {
+		var pictureFile = pictures[Math.floor(Math.random() * pictures.length)];
+		console.log("Loading picture ", pictureFile);
+
+		var picture = document.getElementById("picture");
+		picture.src = 'images/' + pictureFile;
+
+	};
+
+	function onKeyPressed(key) {
+		var letter = keyToLetter(key);
+		console.log(letter);
+	};
+
+	window.addEventListener("keypress", function(event) {
+		onKeyPressed(event.key);
+	});
+
+	loadPictures();
 });
