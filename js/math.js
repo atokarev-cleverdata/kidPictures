890ea9f1aab7484d04332b0c82840cecf0e0f1ea
Alexander Tokarev <alex.tokarew@gmail.com>
2019-03-25 18:17:28 +0300

    Math game process

--- js/math.js
+++ js/math.js
@@ -1,5 +1,7 @@
 var game = function() {
 
+	var limit = 9;
+
 	var numLeft = 0;
 	var numRight = 0;
 	var numSum = 0;
@@ -7,8 +9,11 @@ var game = function() {
 	var position = 0;
 	var totalLength = 0;
 	var errorsCount = 0;
+	var digits = [];
+	var digitValues = [];
 
 	var potSnippet = '<img src="assets/honeypot.png" height="70">';
+	var digitSnippet = '<span>_</span>';
 
 	function keyToDigit(key) {
 		var result = parseInt(key, 10);
@@ -21,11 +26,6 @@ var game = function() {
 		}
 	}
 
-	function updateScreen(elemId, digit, isError) {
-		
-		document.getElementById(elemId);
-	}
-
 	return {
 		onKeyPressed: function(key) {
 			if (position < totalLength) {
@@ -34,12 +34,12 @@ var game = function() {
 					return;
 				}
 
-				if (position == 0) {
-					updateScreen("num-left", digit, digit == numLeft);
-				} else if (position == 1) {
-					updateScreen("num-right", digit, digit == numRight);
-				} else {
+				var span = digits[position];
 
+				span.textContent = digit;
+				if (digitValues[position] != digit) {
+					errorsCount++;
+					span.style.color = 'red';
 				}
 
 				position++;
@@ -47,19 +47,48 @@ var game = function() {
 			}
 		},
 		handleBackSpace: function() {
-			console.log("backspace");
+			if (position > 0) {
+				position--;
+				var span = digits[position];
+				var existingDigit = parseInt(span.textContent, 10);
+				
+				if (existingDigit != digitValues[position]) {
+					errorsCount--;
+					span.style.color = 'black';
+				}
+
+				span.textContent = '_';
+			}
 		},
 		onLoad: function() {
-			numLeft = Math.floor(Math.random() * 9) + 1;
-			numRight = Math.floor(Math.random() * 9) + 1;
+			numLeft = Math.floor(Math.random() * limit) + 1;
+			numRight = Math.floor(Math.random() * limit) + 1;
 			numSum = numLeft + numRight;
 
-			document.getElementById("num-left").textContent = "_";
-			document.getElementById("num-right").textContent = "_";
-			document.getElementById("num-sum").textContent = numSum > 9 ? "__" : "_";
+			document.getElementById("num-left").innerHTML = digitSnippet;
+			document.getElementById("num-right").innerHTML = digitSnippet;
+			document.getElementById("num-sum").innerHTML = digitSnippet.repeat(numSum > 9 ? 2 : 1);
 
 			document.getElementById("pot-left").innerHTML = potSnippet.repeat(numLeft);
 			document.getElementById("pot-right").innerHTML = potSnippet.repeat(numRight);
+
+			digitValues = [numLeft, numRight];
+			if (numSum > 9) {
+				digitValues.push(Math.floor(numSum / 10));
+				digitValues.push(numSum % 10);
+			} else {
+				digitValues.push(numSum);
+			}
+
+			digits = [
+				document.getElementById("num-left").childNodes[0],
+				document.getElementById("num-right").childNodes[0],
+				document.getElementById("num-sum").childNodes[0]
+			];
+
+			if (numSum > 9) {
+				digits.push(document.getElementById("num-sum").childNodes[1])
+			}
 
 			position = 0;
 			totalLength = numSum > 9 ? 4 : 3;
